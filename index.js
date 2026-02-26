795531d775bdd7690a2989385fa087429726fecc
mdx1230 <ii1452671@gmail.com>
2026-02-26 16:11:15 +0300

    task 3 added

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ for(let i=1;i<=a;i++)
 	sum*=i;
 }
 console.log(sum);
+//задание 3
+class T
+{
+  product;
+	price;
+}
+let array={
+	new: T("prod1",30),
+	new: T("prod2",70),
+	new: T("prod3",55)
+}
+let sum=0;
+let average=0;
+function getAverage(mas)
+{
+	for(let i=0;i<mas.length;i++)
+	{
+		sum+=mas[i.price];
+	}
+	average=sum/mas.length;
+	console.log("Средняя цена:",average);
+}
+getAverage(array);
